7e70a73c26002503ec2925e42e482e9ba0298cad
sreeranj2021 <91444378+sreeranj2021@users.noreply.github.com>
2022-02-02 19:29:46 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/sh
 
-echo "hello" >> /home/sysadmin/scriptoutput.txt
+while getopts u:a: flag
+do
+    case "${flag}" in
+        u) hash=${OPTARG};;
+        a) branch=${OPTARG};;
+    esac
+done
+
+echo "$hash" >> /home/sysadmin/scriptoutput.txt
+echo "$branch" >> /home/sysadmin/scriptoutput.txt
